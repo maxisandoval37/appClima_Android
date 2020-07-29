04e55513bda2ed5fd2be3e2bb86ec30e6e4dcd08
Maximiliano Sandoval <maxisandoval98@gmail.com>
2020-07-29 13:33:34 -0300

    agregado intent para acceder a la pag principal

--- app/src/main/java/com/mxdigitalacademy/clima/Ciudad.kt
+++ app/src/main/java/com/mxdigitalacademy/clima/Ciudad.kt
@@ -1,11 +1,27 @@
 package com.mxdigitalacademy.clima
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class Ciudad : AppCompatActivity() {
+
+    fun accionBotonContinuar(){
+        val boton = findViewById<Button>(R.id.btContinuar)
+
+        boton.setOnClickListener(View.OnClickListener {
+            val IntentMainClass = Intent(this,MainActivity::class.java) //a donde, nos redirigimos
+            startActivity(IntentMainClass)
+
+        })
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cuidad)
+
+        accionBotonContinuar()
     }
 }
